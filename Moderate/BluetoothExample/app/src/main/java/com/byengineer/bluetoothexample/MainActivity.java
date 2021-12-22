4ec82ca6a7e32a9e27bb10c3ec8a824c1c36dc8d
Orhan Bozgeyik <orhanbzgyk17@gmail.com>
2021-12-22 22:27:27 +0300

    Bluetooth Using
    How to use Bluetooth?

---
+++ Moderate/BluetoothExample/app/src/main/java/com/byengineer/bluetoothexample/MainActivity.java
@@ -0,0 +1,59 @@
+package com.byengineer.bluetoothexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_ENABLE_BT=0;
+    private static final int REQUEST_DISCOVERABLE_BT=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView out=findViewById(R.id.out);
+        final Button button1=findViewById(R.id.button1);
+        final Button button2=findViewById(R.id.button2);
+        final Button button3=findViewById(R.id.button3);
+        final BluetoothAdapter bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter==null)
+            out.append("device not supported");
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!bluetoothAdapter.isEnabled()){
+                    Intent enableBtIntent=new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent,REQUEST_ENABLE_BT);
+                }
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!bluetoothAdapter.isDiscovering()) {
+                    //out.append("MAKING YOUR DEVICE DISCOVERABLE");
+                    Toast.makeText(getApplicationContext(), "MAKING YOUR DEVICE DISCOVERABLE", Toast.LENGTH_LONG).show();
+                    Intent enableBtIntent=new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    startActivityForResult(enableBtIntent,REQUEST_DISCOVERABLE_BT);
+                }
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    bluetoothAdapter.disable();
+                    //out.append("TURN_OFF BLUETOOTH");
+                    Toast.makeText(getApplicationContext(), "TURN_OFF BLUETOOTH", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+}
